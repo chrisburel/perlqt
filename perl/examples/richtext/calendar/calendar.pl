532b18db70fcca42390e3f70b25b03020c822bef
Fathi Boudra <fabo@kde.org>
2010-02-16 21:07:08 +0000

    [perlqt4 @ 381] Adding richtext/calendar example.
    Original author: chrisburel
    Date: 2009-09-24 06:03:14.595985+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091351

---
+++ perl/examples/richtext/calendar/calendar.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Qt;
+use MainWindow;
+
+sub main
+{
+    my $app = Qt::Application(\@ARGV);
+    my $window = MainWindow();
+    $window->resize(640, 256);
+    $window->show();
+    return $app->exec();
+}
+
+exit main();
