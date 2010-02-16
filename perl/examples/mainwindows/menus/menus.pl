bd56b9b7f324e09ffe197255dd6de7b191fc42c6
Fathi Boudra <fabo@kde.org>
2010-02-16 20:15:29 +0000

    [perlqt4 @ 279] Adding mainwindows/menus example
    Original author: chrisburel
    Date: 2009-08-22 06:22:58.912244+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091263

---
+++ perl/examples/mainwindows/menus/menus.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Qt;
+
+use MainWindow;
+
+sub main {
+    my $app = Qt::Application( \@ARGV );
+    my $window = MainWindow();
+    $window->show();
+    return $app->exec();
+}
+
+exit main();
