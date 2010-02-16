fab368549d0f5646a259d7596f3a5b1ceca0f34f
Fathi Boudra <fabo@kde.org>
2010-02-16 21:17:44 +0000

    [perlqt4 @ 446] Add richtext/syntaxhighlighter example. Update wiki page status of richtext/syntaxhighlighter and richtext/orderform examples.
    Original author: chrisburel
    Date: 2009-12-03 06:54:53.350404+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091410

---
+++ perl/examples/richtext/syntaxhighlighter/Highlighter.pm
@@ -0,0 +1,162 @@
+package HighlightingRule;
+
+sub new {
+    my ( $class, $pattern, $format ) = @_;
+    return bless { pattern => $pattern, format => $format }, $class;
+}
+
+sub pattern {
+    return shift->{pattern};
+}
+
+sub format {
+    return shift->{format};
+}
+
+package Highlighter;
+
+use strict;
+use warnings;
+use Qt4;
+
+# [0]
+use Qt4::isa qw( Qt4::SyntaxHighlighter );
+
+=begin
+    struct HighlightingRule
+    {
+        Qt4::RegExp pattern;
+        Qt4::TextCharFormat format;
+    };
+    Qt4::Vector<HighlightingRule> highlightingRules;
+
+=cut
+# [0]
+
+# [0]
+sub NEW
+{
+    my ( $class, $parent ) = @_;
+    $class->SUPER::NEW($parent);
+
+    this->{keywordFormat} = Qt4::TextCharFormat();
+    this->{classFormat} = Qt4::TextCharFormat();
+    this->{singleLineCommentFormat} = Qt4::TextCharFormat();
+    this->{multiLineCommentFormat} = Qt4::TextCharFormat();
+    this->{quotationFormat} = Qt4::TextCharFormat();
+    this->{functionFormat} = Qt4::TextCharFormat();
+
+    this->{keywordFormat}->setForeground(Qt4::darkBlue());
+    this->{keywordFormat}->setFontWeight(Qt4::Font::Bold());
+    my @keywordPatterns = (
+                    "\\bchar\\b", "\\bclass\\b", "\\bconst\\b",
+                    "\\bdouble\\b", "\\benum\\b", "\\bexplicit\\b",
+                    "\\bfriend\\b", "\\binline\\b", "\\bint\\b",
+                    "\\blong\\b", "\\bnamespace\\b", "\\boperator\\b",
+                    "\\bprivate\\b", "\\bprotected\\b", "\\bpublic\\b",
+                    "\\bshort\\b", "\\bsignals\\b", "\\bsigned\\b",
+                    "\\bslots\\b", "\\bstatic\\b", "\\bstruct\\b",
+                    "\\btemplate\\b", "\\btypedef\\b", "\\btypename\\b",
+                    "\\bunion\\b", "\\bunsigned\\b", "\\bvirtual\\b",
+                    "\\bvoid\\b", "\\bvolatile\\b" );
+    foreach my $pattern (@keywordPatterns) {
+        my $rule = HighlightingRule->new(
+            Qt4::RegExp($pattern),
+            this->{keywordFormat}
+        );
+        push @{this->{highlightingRules}}, $rule;
+# [0] //! [1]
+    }
+# [1]
+
+# [2]
+    this->{classFormat}->setFontWeight(Qt4::Font::Bold());
+    this->{classFormat}->setForeground(Qt4::darkMagenta());
+    push @{this->{highlightingRules}},
+        HighlightingRule->new(
+            Qt4::RegExp("\\bQ[A-Za-z]+\\b"),
+            this->{classFormat}
+        );
+# [2]
+
+# [3]
+    this->{singleLineCommentFormat}->setForeground(Qt4::red());
+    
+    push @{this->{highlightingRules}},
+        HighlightingRule->new(
+            Qt4::RegExp("//[^\n]*"),
+            this->{singleLineCommentFormat}
+        );
+
+    this->{multiLineCommentFormat}->setForeground(Qt4::red());
+# [3]
+
+# [4]
+    this->{quotationFormat}->setForeground(Qt4::darkGreen());
+    push @{this->{highlightingRules}},
+        HighlightingRule->new(
+            Qt4::RegExp("\'.*\'"),
+            this->{quotationFormat}
+        );
+# [4]
+
+# [5]
+    this->{functionFormat}->setFontItalic(1);
+    this->{functionFormat}->setForeground(Qt4::blue());
+    push @{this->{highlightingRules}},
+        HighlightingRule->new(
+            Qt4::RegExp("\\b[A-Za-z0-9_]+(?=\\()"),
+            this->{functionFormat}
+        );
+# [5]
+
+# [6]
+    this->{commentStartExpression} = Qt4::RegExp("/\\*");
+    this->{commentEndExpression} = Qt4::RegExp("\\*/");
+}
+# [6]
+
+# [7]
+sub highlightBlock
+{
+    $DB::single=1;
+    my ($text) = @_;
+    foreach my $rule ( @{this->{highlightingRules}} ) {
+        my $expression = Qt4::RegExp($rule->pattern());
+        my $index = $expression->indexIn($text);
+        while ($index >= 0) {
+            my $length = $expression->matchedLength();
+            this->setFormat($index, $length, $rule->format());
+            $index = $expression->indexIn($text, $index + $length);
+        }
+    }
+# [7] //! [8]
+    this->setCurrentBlockState(0);
+# [8]
+
+# [9]
+    my $startIndex = 0;
+    if (this->previousBlockState() != 1) {
+        $startIndex = this->{commentStartExpression}->indexIn($text);
+    }
+
+# [9] //! [10]
+    while ($startIndex >= 0) {
+        $DB::single=1;
+# [10] //! [11]
+        my $endIndex = this->{commentEndExpression}->indexIn($text, $startIndex);
+        my $commentLength;
+        if ($endIndex == -1) {
+            this->setCurrentBlockState(1);
+            $commentLength = length( $text ) - $startIndex;
+        } else {
+            $commentLength = $endIndex - $startIndex
+                            + this->{commentEndExpression}->matchedLength();
+        }
+        this->setFormat($startIndex, $commentLength, this->{multiLineCommentFormat});
+        $startIndex = this->{commentStartExpression}->indexIn($text, $startIndex + $commentLength);
+    }
+}
+# [11]
+
+1;
