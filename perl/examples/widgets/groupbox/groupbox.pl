2464c8c557132fa8a3e0e30ad1859c4e1484541f
Fathi Boudra <fabo@kde.org>
2010-02-16 20:07:25 +0000

    [perlqt4 @ 217] Adding widgets/groupbox example.
    Original author: chrisburel
    Date: 2009-06-10 07:27:10.393252+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091218

---
+++ perl/examples/widgets/groupbox/groupbox.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use blib;
+
+use Qt;
+use Window;
+
+sub main {
+    my $app = Qt::Application( \@ARGV );
+    my $window = Window();
+    $window->show();
+    exit $app->exec();
+}
+
+main();
