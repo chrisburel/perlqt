d00db4264cb143d2bf4fdc346177084dc33f3beb
Fathi Boudra <fabo@kde.org>
2010-02-16 21:03:37 +0000

    [perlqt4 @ 345] Adding layouts/flowlayout example.
    Original author: chrisburel
    Date: 2009-09-18 07:36:58.114444+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091328

---
+++ perl/examples/layouts/flowlayout/Window.pm
@@ -0,0 +1,29 @@
+package Window;
+
+use strict;
+use warnings;
+use Qt;
+# [0]
+use Qt::isa qw( Qt::Widget );
+# [0]
+use FlowLayout;
+
+# [1]
+sub NEW
+{
+    my ($class) = @_;
+    $class->SUPER::NEW();
+    my $flowLayout = FlowLayout();
+
+    $flowLayout->addWidget(Qt::PushButton(this->tr('Short')));
+    $flowLayout->addWidget(Qt::PushButton(this->tr('Longer')));
+    $flowLayout->addWidget(Qt::PushButton(this->tr('Different text')));
+    $flowLayout->addWidget(Qt::PushButton(this->tr('More text')));
+    $flowLayout->addWidget(Qt::PushButton(this->tr('Even longer button text')));
+    this->setLayout($flowLayout);
+
+    this->setWindowTitle(this->tr('Flow Layout'));
+}
+# [1]
+
+1;
