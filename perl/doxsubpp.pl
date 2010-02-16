32cc1910d1eeb041400af1d387fb139ecae99c73
Fathi Boudra <fabo@kde.org>
2010-02-16 19:48:20 +0000

    [perlqt4 @ 166] Forgot this in the last commit.
    Original author: chrisburel
    Date: 2009-05-11 06:43:42.957080+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091182

---
+++ perl/doxsubpp.pl
@@ -0,0 +1,22 @@
+
+use strict;
+use warnings;
+use ExtUtils::MakeMaker;
+
+my $perl = $ARGV[0];
+
+my $mm = ExtUtils::MakeMaker->new( {
+    NAME => 'PerlQt',
+    NEEDS_LINKING => 1,
+} );
+
+my $perl_include_path = $mm->{PERL_INC};
+my @xsubinfo = split "\n", $mm->tool_xsubpp();
+
+my $xsubppdir = (map{ my $foo = $_; $foo =~ s/XSUBPPDIR = //; $foo } grep{ m/^XSUBPPDIR =/ } @xsubinfo)[0];
+my $xsubpp = "$xsubppdir/xsubpp";
+
+my $xsubppargs = (map{ my $foo = $_; $foo =~ s/XSUBPPARGS = //; $foo } grep{ m/^XSUBPPARGS =/ } @xsubinfo)[0];
+
+my $cmd = "$perl $xsubpp Qt.xs $xsubppargs > Qt.cpp";
+system $cmd;
