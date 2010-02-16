e96073747d4b6ac068771b90c7a31af15c49b11c
Fathi Boudra <fabo@kde.org>
2010-02-16 20:15:38 +0000

    [perlqt4 @ 280] Adding mainwindows/recentfiles example.
    Original author: chrisburel
    Date: 2009-08-22 07:10:39.029243+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091264

---
+++ perl/examples/mainwindows/recentfiles/recentfiles.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Qt;
+
+use MainWindow;
+
+sub main {
+    my $app = Qt::Application( \@ARGV );
+    my $mainWin = MainWindow();
+    $mainWin->show();
+    return $app->exec();
+}
+
+exit main();
