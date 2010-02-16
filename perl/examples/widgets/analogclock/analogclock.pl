4091d28d167698f34c3e7a46e3474a0dd935cc36
Fathi Boudra <fabo@kde.org>
2010-02-16 20:04:12 +0000

    [perlqt4 @ 179] Adding widgets/analogclock example.
    Original author: chrisburel
    Date: 2009-05-24 02:58:36.352944+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091195

---
+++ perl/examples/widgets/analogclock/analogclock.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use blib;
+
+use Qt;
+
+use AnalogClock;
+
+sub main {
+    my $app = Qt::Application( \@ARGV );
+    my $clock = AnalogClock();
+    $clock->show();
+    exit $app->exec();
+}
+
+main();
