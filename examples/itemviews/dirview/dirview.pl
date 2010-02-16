200f84525d5af4d9a87aff1f87eeb700ae3962cd
Fathi Boudra <fabo@kde.org>
2010-02-16 19:43:27 +0000

    [perlqt4 @ 141] Added itemviews/dirview example.
    Original author: chrisburel
    Date: 2009-04-08 18:59:33.672822+00:00
    
    svn path=/trunk/playground/bindings/perlqt4/; revision=1091157

---
+++ examples/itemviews/dirview/dirview.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use blib;
+
+use Qt;
+
+my $app = Qt::Application( \@ARGV );
+
+my $model = Qt::DirModel();
+my $tree = Qt::TreeView();
+$tree->setModel($model);
+
+# Demonstrating look and feel features
+$tree->setAnimated(0);
+$tree->setIndentation(20);
+$tree->setSortingEnabled(1);
+
+$tree->setWindowTitle(Qt::Object::tr('Dir View'));
+$tree->resize(640, 480);
+$tree->show();
+
+exit $app->exec();
