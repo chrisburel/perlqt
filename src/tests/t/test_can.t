cbd0a2a0c840d753579ff3b94c54bdc985709dea
Chris Burel <chrisburel@gmail.com>
2016-09-29 09:57:53 -0700

    Correct can() for perl subclasses.

--- src/tests/t/test_can.t
+++ src/tests/t/test_can.t
@@ -1,4 +1,4 @@
-use Test::More tests => 6;
+use Test::More tests => 13;
 
 use strict;
 use warnings;
@@ -10,7 +10,7 @@ ok(defined $ctor);
 
 my $app = $ctor->('PerlSmokeTest::QApplication');
 ok(defined $app);
-is(ref $app, 'PerlSmokeTest::QApplication');
+isa_ok($app, 'PerlSmokeTest::QApplication');
 
 my $instanceSub = $app->can('instance');
 ok(defined $instanceSub);
@@ -20,3 +20,36 @@ is($app, $app2);
 
 my $cant = $app->can('nonexistantMethod');
 ok(!defined $cant);
+
+package Application;
+
+use base qw(PerlSmokeTest::QApplication);
+
+sub new {
+    my ($class) = @_;
+    my $self = $class->SUPER::new();
+    $self->{callCount} += 1;
+    return $self;
+}
+
+package main;
+
+$ctor = Application->can('new');
+ok(defined $ctor);
+
+$app = $ctor->('Application');
+isa_ok($app, 'Application');
+is($app->{callCount}, 1);
+
+$instanceSub = $app->can('instance');
+ok(defined $instanceSub);
+
+$app2 = $instanceSub->($app);
+is($app, $app2);
+
+my $fakeApp = bless {}, 'Application';
+ok(!defined $fakeApp->can('new'));
+
+my $one = 1;
+$fakeApp = bless \$one, 'Application';
+ok(!defined $fakeApp->can('new'));
--- src/xsfunctions.cpp
+++ src/xsfunctions.cpp
@@ -57,34 +57,41 @@ XS(XS_AUTOLOAD) {
 XS(XS_CAN) {
     dXSARGS;
     const char* methodName = SvPVX(ST(1));
-    const char* package;
     SV* self = ST(0);
+    Smoke::ModuleIndex classId;
+    HV* stash;
+
     if (SvTYPE(self) == SVt_PV) {
-       package = SvPVX(self);
+        const char* package = SvPVX(self);
+        stash = gv_stashpv(package, 0);
+        std::string className = SmokePerl::SmokeManager::instance().getClassForPackage(package);
+        if (className == "") {
+            AV* mro = mro_get_linear_isa(stash);
+            for (int i=0; i < av_len(mro), className == ""; ++i) {
+                SV** item = av_fetch(mro, i, 0);
+                className = SmokePerl::SmokeManager::instance().getClassForPackage(SvPV_nolen(*item));
+            }
+        }
+        classId = Smoke::findClass(className.c_str());
     }
     else {
-        if (!SvROK(self))
+        // self must be a reference
+        stash = SvSTASH(SvRV(self));
+        SmokePerl::Object* obj = SmokePerl::Object::fromSV(self);
+        if (obj == nullptr)
             XSRETURN(0);
-        HV* stash = SvSTASH(SvRV(self));
-        if (stash == nullptr)
-            XSRETURN(0);
-        package = HvNAME(stash);
+        classId = obj->classId;
     }
-    Smoke* smoke = SmokePerl::SmokeManager::instance().getSmokeForPackage(package);
-    if (smoke == nullptr)
-        XSRETURN(0);
-    std::string className = SmokePerl::SmokeManager::instance().getClassForPackage(package);
-    if (className == "")
-        XSRETURN(0);
+
+    const char* className = classId.smoke->classes[classId.index].className;
+
     bool isConstructor = strcmp(methodName, "new") == 0;
-    Smoke::ModuleIndex method = smoke->findMethodName(className.c_str(), isConstructor ? className.c_str() : methodName);
+    Smoke::ModuleIndex method = classId.smoke->findMethodName(className, isConstructor ? className : methodName);
+
     if (method.index) {
-        HV* stash = gv_stashpv(package, 0);
         GV* autoload = gv_fetchmethod_autoload(stash, methodName, 1);
-        if (autoload) {
-            ST(0) = newRV_noinc((SV*)GvCV(autoload));
-            XSRETURN(1);
-        }
+        ST(0) = newRV_noinc((SV*)GvCV(autoload));
+        XSRETURN(1);
     }
     XSRETURN(0);
 }
