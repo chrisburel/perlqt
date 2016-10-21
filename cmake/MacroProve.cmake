631a6f137a89f68b92b053c484032de2a5c7d8a7
Chris Burel <chrisburel@gmail.com>
2016-10-20 17:30:27 -0700

    Adjust prove function to run /path/to/perl /path/to/prove args
    This allows valgrind to run correctly on the tests.

--- cmake/MacroProve.cmake
+++ cmake/MacroProve.cmake
@@ -1,23 +1,32 @@
 
 FUNCTION( MACRO_PROVE _testname _path)
 
+    get_filename_component(perl_basedir ${PERL_EXECUTABLE} DIRECTORY)
+
+    find_program(
+        prove_cmd
+        NAMES prove prove.bat
+        HINTS ${perl_basedir}
+    )
+
     if(WIN32)
-        set(prove_cmd "prove.bat")
         string(REGEX REPLACE " " "\\\\\\\\ " escaped_path ${_path})
     else()
-        set(prove_cmd "prove")
         set(escaped_path ${_path})
     endif(WIN32)
 
+    set(prove_args ${prove_cmd})
     if ( USE_BUILD_DIR_FOR_TESTS )
-        set(prove_args -I${CMAKE_BINARY_DIR}/blib/lib -I${CMAKE_BINARY_DIR}/blib/arch)
+        list(APPEND prove_args -I${CMAKE_BINARY_DIR}/blib/lib -I${CMAKE_BINARY_DIR}/blib/arch)
     endif ( USE_BUILD_DIR_FOR_TESTS )
+
+    set(exe ${PERL_EXECUTABLE})
 
     set(_workingdir ${ARGV2})
     if(_workingdir)
-        set(prove_args -E chdir ${_workingdir} ${prove_cmd} ${prove_args})
-        set(prove_cmd ${CMAKE_COMMAND})
+        set(prove_args -E chdir ${_workingdir} ${PERL_EXECUTABLE} ${prove_cmd} ${prove_args})
+        set(exe ${CMAKE_COMMAND})
     endif(_workingdir)
 
-    add_test(${_testname} ${prove_cmd} ${prove_args} ${escaped_path})
+    add_test(${_testname} ${exe} ${prove_args} ${escaped_path})
 ENDFUNCTION( MACRO_PROVE _testname _path )
