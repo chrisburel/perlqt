e6d18835e3af6192d37b27c3f1e1c7816fe5c18c
Chris Burel <chrisburel@gmail.com>
2017-08-21 16:55:37 -0700

    Add QtWebEngineCore module

---
+++ modules/qtwebenginecore/QtWebEngineCore.pm
@@ -0,0 +1,18 @@
+package PerlQt5::QtWebEngineCore;
+
+use strict;
+use warnings;
+use XSLoader;
+use PerlQt5::QtQuick;
+use PerlQt5::QtGui;
+use PerlQt5::QtNetwork;
+
+our $VERSION = '1.0.0';
+
+PerlQt5::QtCore::loadModule(__PACKAGE__, $VERSION);
+
+sub import {
+    goto &PerlQt5::QtCore::import;
+}
+
+1;
