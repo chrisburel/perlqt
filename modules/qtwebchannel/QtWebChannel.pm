3486a44b017859e76bfe5332a4806416c219b455
Chris Burel <chrisburel@gmail.com>
2017-08-21 22:43:22 -0700

    Add QtWebChannel module

--- modules/qtwebchannel/QtWebChannel.pm
+++ modules/qtwebchannel/QtWebChannel.pm
@@ -1,13 +1,9 @@
-package PerlQt5::QtWebEngineCore;
+package PerlQt5::QtWebChannel;
 
 use strict;
 use warnings;
 use XSLoader;
-use PerlQt5::QtQuick;
 use PerlQt5::QtGui;
-use PerlQt5::QtNetwork;
-use PerlQt5::QtPositioning;
-use PerlQt5::QtWebChannel;
 
 our $VERSION = '1.0.0';
 
